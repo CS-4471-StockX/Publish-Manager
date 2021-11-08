8d12dcf5d8896a008cc6369998439964edb3b1df
wanger26 <jwanger@uwo.ca>
2021-11-08 10:47:42 -0500

    Inital upload

---
+++ src/main/java/com/stockx/publishmanagerws/PublishManagerWsApplication.java
@@ -0,0 +1,13 @@
+package com.stockx.publishmanagerws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishManagerWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishManagerWsApplication.class, args);
+	}
+
+}
