a0a67ce3e7793b821faacc4b6bfaf91a8f135f2a
wanger26 <jwanger@uwo.ca>
2021-11-10 12:13:15 -0500

    Added functionality to publish messages to AWS IoT

--- src/main/java/com/stockx/publishmanagerws/controllers/PublishManagerController.java
+++ src/main/java/com/stockx/publishmanagerws/controllers/PublishManagerController.java
@@ -2,9 +2,7 @@ package com.stockx.publishmanagerws.controllers;
 
 import com.stockx.publishmanagerws.services.PublishManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PublishManagerController {
@@ -13,7 +11,7 @@ public class PublishManagerController {
     private PublishManagerService publishManagerService;
 
     @PostMapping(value = "/publish")
-    public void publishStockListing(@RequestParam("ticker") String topic, String message) {
-        publishManagerService.publishStockListing(topic, message);
+    public void publishStockListing(@RequestParam("topic") String topic, @RequestBody String message) {
+        publishManagerService.publishMessage(topic, message);
     }
 }
